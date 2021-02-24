bf5891c38294a6bca4e16122d1be8a6dde7b0b87
jacob7140 <jacobellissmith@gmail.com>
2021-02-24 15:26:31 -0500

    Initial commit

---
+++ app/src/main/java/com/example/itunes_app/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.itunes_app;
+/*inclass 4
+ * @author Shaishav Maisuria
+ * @jacob smith
+ */
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements LoginFragment.LoginListener, RegisterFragment.SignupListener, AccountFragment.AccountListener
+        ,UpdateFragment.UpdateListener
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.rootView,new LoginFragment())
+                .commit();
+    }
+
+    DataServices.Account mAccount;
+
+    @Override
+    public void loginIsSuccessful(DataServices.Account account) {
+        this.mAccount= account;
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.rootView, AccountFragment.newInstance(this.mAccount),"accountFragment")
+                .commit();
+    }
+
+    @Override
+    public void gotoLogin() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.rootView, new LoginFragment())
+                .commit();
+    }
+
+    @Override
+    public void gotoRegistration() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.rootView, new RegisterFragment())
+                .commit();
+    }
+
+    @Override
+    public void gotoUpdate() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.rootView,UpdateFragment.newInstance(this.mAccount))
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void logout() {
+        this.mAccount = null;
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.rootView, new LoginFragment())
+                .commit();
+    }
+
+    @Override
+    public void updateToAccount(DataServices.Account account) {
+        this.mAccount=account;
+
+        AccountFragment accountFragment= (AccountFragment) getSupportFragmentManager().findFragmentByTag("accountFragment");
+        if(accountFragment!=null) {
+            accountFragment.setAccountDetails(mAccount);
+        }
+
+        getSupportFragmentManager().popBackStack();
+
+
+    }
+
+    @Override
+    public void backToAccount() {
+
+        getSupportFragmentManager().popBackStack();
+    }
+}
---
+++ app/src/main/java/com/example/itunes_app/RegisterFragment.java
@@ -0,0 +1,104 @@
+package com.example.itunes_app;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class RegisterFragment extends Fragment {
+
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    TextView userName;
+    TextView userEmail;
+    TextView userPassword;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        getActivity().setTitle("Register Account");
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        userName = view.findViewById(R.id.editextPersonNameRegister);
+        userEmail = view.findViewById(R.id.editTextEmailAddressRegister);
+        userPassword = view.findViewById(R.id.editTextPasswordRegister);
+
+
+        view.findViewById(R.id.buttonSubmitRegister).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = userName.getText().toString();
+                String email = userEmail.getText().toString();
+                String password = userPassword.getText().toString();
+                Log.d("register", "name" + name);
+                Log.d("register", "email" + email);
+                Log.d("register", "password" + password);
+
+
+                if (email.isEmpty() || password.isEmpty() || name.isEmpty()) {
+                    Toast.makeText(getActivity(), "Password/Email or Password cannot be empty", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    DataServices.Account account = DataServices.register(name, email, password);
+                    if( account!=null){
+                        Toast.makeText(getActivity(), "Successfull Registration", Toast.LENGTH_SHORT).show();
+                        mListener.loginIsSuccessful(account);
+                    }else {
+
+                        Toast.makeText(getActivity(), "UnSuccessfull Registration", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+
+
+        view.findViewById(R.id.buttonCancelRegister).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.gotoLogin();
+            }
+        });
+
+        return view;
+    }
+
+
+    SignupListener mListener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if(context instanceof SignupListener) {
+            mListener = (SignupListener) context;
+        }else{
+            throw  new RuntimeException((context.toString()+"must implement SignupListener"));
+        }
+    }
+
+    interface SignupListener {
+        void loginIsSuccessful(DataServices.Account account);
+        void gotoLogin();
+    }
+}
